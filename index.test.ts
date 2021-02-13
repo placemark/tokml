6ce8ddb0c1b42788892755124fa65ca69af09e3c
Tom MacWright <tom@macwright.org>
2021-02-13 15:19:11 -0800

    Complete geometry coverage

--- index.test.ts
+++ index.test.ts
@@ -1,13 +1,13 @@
 import { test } from "tap";
-import { hexToKmlColor, toKML } from "./";
+import { toKML } from "./";
 
-test("hexToKmlColor", (t) => {
-  t.same(hexToKmlColor("123", 1), "ff332211");
-  t.same(hexToKmlColor("123", 0.5), "7f332211");
-  t.same(hexToKmlColor("123", 0.05), "0c332211");
-  t.same(hexToKmlColor("aaccdd", 1), "ffddccaa");
-  t.end();
-});
+// test("hexToKmlColor", (t) => {
+//   t.same(hexToKmlColor("123", 1), "ff332211");
+//   t.same(hexToKmlColor("123", 0.5), "7f332211");
+//   t.same(hexToKmlColor("123", 0.05), "0c332211");
+//   t.same(hexToKmlColor("aaccdd", 1), "ffddccaa");
+//   t.end();
+// });
 
 test("toKML", (t) => {
   t.same(
@@ -52,7 +52,7 @@ test("toKML", (t) => {
         },
       ],
     }),
-    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><Point><coordinates>0, 2</coordinates></Point></Placemark><Placemark><MultiGeometry><Point><coordinates>0, 2</coordinates></Point><Point><coordinates>1, 2</coordinates></Point></MultiGeometry></Placemark><Placemark><LineString><coordinates>0, 2\n1, 2</coordinates></LineString></Placemark></Document></kml>`
+    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><ExtendedData><Data name=\"foo\"><value>bar</value></Data></ExtendedData><Point><coordinates>0, 2</coordinates></Point></Placemark><Placemark><ExtendedData><Data name=\"foo\"><value>bar</value></Data></ExtendedData><MultiGeometry><Point><coordinates>0, 2</coordinates></Point><Point><coordinates>1, 2</coordinates></Point></MultiGeometry></Placemark><Placemark><ExtendedData><Data name=\"foo\"><value>bar</value></Data></ExtendedData><LineString><coordinates>0, 2\n1, 2</coordinates></LineString></Placemark></Document></kml>`
   );
 
   t.same(
@@ -84,7 +84,7 @@ test("toKML", (t) => {
         },
       ],
     }),
-    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><Polygon><outerBoundaryIs><LinearRing><coordinates>0, 2\n1, 2\n2, 2\n0, 2</coordinates></LinearRing></outerBoundaryIs><innerBoundaryIs><LinearRing><coordinates>0, 3\n1, 3\n2, 3\n0, 3</coordinates></LinearRing></innerBoundaryIs></Polygon></Placemark></Document></kml>`
+    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><ExtendedData><Data name=\"foo\"><value>bar</value></Data></ExtendedData><Polygon><outerBoundaryIs><LinearRing><coordinates>0, 2\n1, 2\n2, 2\n0, 2</coordinates></LinearRing></outerBoundaryIs><innerBoundaryIs><LinearRing><coordinates>0, 3\n1, 3\n2, 3\n0, 3</coordinates></LinearRing></innerBoundaryIs></Polygon></Placemark></Document></kml>`
   );
 
   t.same(
@@ -116,7 +116,7 @@ test("toKML", (t) => {
         },
       ],
     }),
-    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><MultiGeometry><LineString><coordinates>0, 2\n1, 2\n2, 2\n0, 2</coordinates></LineString><LineString><coordinates>0, 3\n1, 3\n2, 3\n0, 3</coordinates></LineString></MultiGeometry></Placemark></Document></kml>`
+    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><ExtendedData><Data name=\"foo\"><value>bar</value></Data></ExtendedData><MultiGeometry><LineString><coordinates>0, 2\n1, 2\n2, 2\n0, 2</coordinates></LineString><LineString><coordinates>0, 3\n1, 3\n2, 3\n0, 3</coordinates></LineString></MultiGeometry></Placemark></Document></kml>`
   );
 
   t.same(
@@ -150,7 +150,7 @@ test("toKML", (t) => {
         },
       ],
     }),
-    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><MultiGeometry><Polygon><outerBoundaryIs><LinearRing><coordinates>0, 2\n1, 2\n2, 2\n0, 2</coordinates></LinearRing></outerBoundaryIs><innerBoundaryIs><LinearRing><coordinates>0, 3\n1, 3\n2, 3\n0, 3</coordinates></LinearRing></innerBoundaryIs></Polygon></MultiGeometry></Placemark></Document></kml>`
+    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><ExtendedData><Data name=\"foo\"><value>bar</value></Data></ExtendedData><MultiGeometry><Polygon><outerBoundaryIs><LinearRing><coordinates>0, 2\n1, 2\n2, 2\n0, 2</coordinates></LinearRing></outerBoundaryIs><innerBoundaryIs><LinearRing><coordinates>0, 3\n1, 3\n2, 3\n0, 3</coordinates></LinearRing></innerBoundaryIs></Polygon></MultiGeometry></Placemark></Document></kml>`
   );
 
   t.same(
@@ -169,7 +169,23 @@ test("toKML", (t) => {
         },
       ],
     }),
-    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><MultiGeometry><Point><coordinates>0, 1</coordinates></Point></MultiGeometry></Placemark></Document></kml>`
+    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><ExtendedData><Data name=\"foo\"><value>bar</value></Data></ExtendedData><MultiGeometry><Point><coordinates>0, 1</coordinates></Point></MultiGeometry></Placemark></Document></kml>`
+  );
+
+  t.same(
+    toKML({
+      type: "FeatureCollection",
+      features: [
+        {
+          type: "Feature",
+          properties: {
+            name: "bar",
+          },
+          geometry: { type: "Point", coordinates: [0, 1] },
+        },
+      ],
+    }),
+    `<kml xmlns=\"http://www.opengis.net/kml/2.2\"><Document><Placemark><name>bar</name><ExtendedData></ExtendedData><Point><coordinates>0, 1</coordinates></Point></Placemark></Document></kml>`
   );
   t.end();
 });
