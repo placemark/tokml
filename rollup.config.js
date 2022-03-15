fed27d9050864664c7b5e7ed73a66df26a60b5fe
Tom MacWright <tom@macwright.org>
2022-03-15 12:38:28 -0400

    docs: Use typedoc, switch to rollup

---
+++ rollup.config.js
@@ -0,0 +1,46 @@
+import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+
+const input = './lib/index.ts';
+const sourcemap = true;
+
+export default [
+  {
+    input,
+    output: {
+      file: 'dist/tokml.es.js',
+      format: 'es',
+      sourcemap,
+    },
+    plugins: [typescript(), nodeResolve()],
+  },
+  {
+    input,
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es',
+    },
+    plugins: [dts()],
+  },
+  {
+    input,
+    output: {
+      file: 'dist/tokml.cjs',
+      format: 'cjs',
+      sourcemap,
+    },
+    plugins: [typescript(), nodeResolve()],
+  },
+  {
+    input,
+    output: {
+      file: 'dist/tokml.umd.js',
+      format: 'umd',
+      name: 'toGeoJSON',
+      sourcemap,
+    },
+    plugins: [typescript(), nodeResolve(), terser()],
+  },
+];
