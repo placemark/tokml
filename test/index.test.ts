cca3e002a8ab54a13485bdf77749233a5f1ec659
Tom MacWright <tmcw@users.noreply.github.com>
2022-10-31 15:52:03 -0400

    chore: Update deps (#26)

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,4 +1,4 @@
-import { test, describe, it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest';
 import { toKML, foldersToKML } from '../lib/index';
 
 describe('foldersToKML', () => {
